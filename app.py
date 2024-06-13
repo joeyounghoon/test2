99670c046672bbb8935568c7bcd82546ea8d2e42
joeyounghoon <yhjoe36511@gmail.com>
2024-06-13 16:39:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ st.sidebar.title('OptimalBotAI')
 
 # 페이지 바 분리를 위한 선픽과 후픽 바 생성
 st.sidebar.markdown('---')  # 선픽
-st.sidebar.write('Additional Options')  # 후픽
 
 # 선픽 이후의 추가 항목 예제
-if st.sidebar.button('Option 1'):
+if st.sidebar.button('선픽'):
     st.sidebar.write("You selected Option 1")
-if st.sidebar.button('Option 2'):
+if st.sidebar.button('후픽'):
     st.sidebar.write("You selected Option 2")
